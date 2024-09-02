3ec8de2a17ceb7709a269482764a265032c3bcb0
om salunke <omsaunke343@gmail.com>
2024-09-02 23:33:41 +0530

    adding profile picture

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Profile_Picture from "./ProfilePicture";
 
 const Intro = () => {
   return (
@@ -7,11 +8,22 @@ const Intro = () => {
         src="./images/bg2.jpg"
         className="w-full h-[100vh] absolute top-0 left-0"
       />
-      <div className="relative bg-green-300 w-[70%] h-[50vh] mx-auto text-white flex items-center">
-        <div className="w-[100%] h-[50%] grid place-items-center  px-6">
-          <h1 className="w-full text-4xl">Hi There <br/> I'm Om Salunke</h1>
+      <div className="relative w-[80%] h-[50vh] mx-auto text-white flex items-center">
+        <div className="w-[100%] h-[50%] grid place-items-start px-6">
+          <h1 className="w-[100%]   text-6xl ">
+            Hi There <br /> I'm{" "}
+            <span className="text-red-500 font-semibold">Om Salunke</span>
+          </h1>
+          <button
+            type="button"
+            className="text-xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 my-4 mb-2"
+          >
+            About Me
+          </button>
         </div>
-        <div className="w-full"></div>
+        <div className="w-fit ">
+          <Profile_Picture />
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/ProfilePicture.tsx
@@ -0,0 +1,8 @@
+export default function Profile_Picture() {
+    return (
+        <div className="box">
+            <div className="img-content"><div className="user-img"><img src="./images/omsalunke.jpeg"/></div></div>
+        </div>
+
+    );
+}
