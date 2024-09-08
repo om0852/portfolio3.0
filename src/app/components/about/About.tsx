b99f679e18ddcadd75a6b7385d0f2cdc1638b169
om salunke <omsaunke343@gmail.com>
2024-09-08 21:54:29 +0530

    about section completed

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import "@/app/globals.css";
+import Link from "next/link";
 const About = () => {
   const [selectDetails, setSelectDetails] = useState(0);
   return (
@@ -57,14 +58,18 @@ const About = () => {
               type="button"
               className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 w-[30vh]"
             >
-              Download CV
+              <a href={"./OM_SALUNKE.pdf"} download="OM-SALUNKE_RESUME.pdf">
+                Download CV
+              </a>
             </button>
-            <button
+            <Link
+            href={"https://www.linkedin.com/in/om-salunke-81bb63292"}
+            target="_blank"
               type="button"
               className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 w-[30vh]"
             >
               Hire Me!
-            </button>
+            </Link>
           </div>
           <div className="">
             <div className="flex flex-row gap-6">
