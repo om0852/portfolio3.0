2fc3bffb08f055a93982ddcb83fe34ea8a1f341a
om salunke <omsaunke343@gmail.com>
2024-09-11 00:21:46 +0530

    some small changes

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -22,6 +22,8 @@ const ProjectCard = ({
       <div className="card-content text-white">
         <h2 className="card-title">{title}</h2>
         <p className="card-body">{description}</p>
+
+        
         <button
           type="button"
           className="mt-6 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
